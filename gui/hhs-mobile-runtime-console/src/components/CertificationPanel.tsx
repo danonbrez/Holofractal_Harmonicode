233b21b53b3860f342a80e4de276b39787a4a35a
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-04-26 14:58:22 -0400

    Add Certification Panel

---
+++ gui/hhs-mobile-runtime-console/src/components/CertificationPanel.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CertificationPanel() {
+  return (
+    <div style={{ padding: 10, fontSize: 11, borderTop: '1px solid #133' }}>
+      <div>Certification</div>
+      <div style={{ color: '#9f9', marginTop: 6 }}>CERTIFIED_BACKEND_READY_FOR_GUI</div>
+    </div>
+  );
+}
