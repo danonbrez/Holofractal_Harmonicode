c9c15f72d5d66a3d8aa5e909c361f92b366b8af1
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-05-13 21:04:24 -0400

    Update App.tsx

--- hhs_gui/src/App.tsx
+++ hhs_gui/src/App.tsx
@@ -1,5 +1,20 @@
-import { RuntimeShell } from "../runtime_os/core/RuntimeShell";
+import React from "react"
+
+import {
+    RuntimeShell
+} from "../runtime_os/core/RuntimeShell"
+
+import {
+    RuntimeOS
+} from "../runtime_os/core/RuntimeOS"
+
+const runtimeOS = new RuntimeOS()
 
 export default function App() {
-  return <RuntimeShell />;
+
+    return (
+        <RuntimeShell
+            runtimeOS={runtimeOS}
+        />
+    )
 }
