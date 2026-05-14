a12d538c00993a3ea9ba9aa77290f6dd28f86cd8
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-05-13 21:02:15 -0400

    Update App.tsx

--- hhs_gui/src/App.tsx
+++ hhs_gui/src/App.tsx
@@ -1,19 +1,3 @@
-import { RuntimeDesktop } from "./runtime_os/core/RuntimeDesktop";
-import { RuntimeWindowManager } from "./runtime_os/core/RuntimeWindowManager";
-
-import { RuntimeCalculator } from "./runtime_apps/calculator/RuntimeCalculator";
-import { RuntimeBreadboard } from "./runtime_apps/breadboard/RuntimeBreadboard";
-
-export default function App() {
-  return (
-    <RuntimeDesktop>
-      <RuntimeWindowManager>
-        <RuntimeCalculator />
-        <RuntimeBreadboard />
-      </RuntimeWindowManager>
-    </RuntimeDesktop>
-  );
-}
 import { RuntimeShell } from "../runtime_os/core/RuntimeShell";
 
 export default function App() {
