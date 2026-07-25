64635a0d676d73b7b51fac773b8d671e47dc32fb
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-07-24 22:27:54 -0400

    Expose Pass 154 NFV runtime surface

---
+++ hhs_runtime/nfv/__init__.py
@@ -0,0 +1,29 @@
+from .core import (
+    CONTRACT_ID,
+    PASS_NUMBER,
+    IMPLEMENTATION_VERSION,
+    LOSHU,
+    LOSHU_TRAVERSAL,
+    NFVError,
+    LocalizedModulus,
+    LocalizedRational,
+    NFVObject,
+    TransitionPackage,
+    hash72,
+    hash216,
+)
+
+__all__ = [
+    "CONTRACT_ID",
+    "PASS_NUMBER",
+    "IMPLEMENTATION_VERSION",
+    "LOSHU",
+    "LOSHU_TRAVERSAL",
+    "NFVError",
+    "LocalizedModulus",
+    "LocalizedRational",
+    "NFVObject",
+    "TransitionPackage",
+    "hash72",
+    "hash216",
+]
