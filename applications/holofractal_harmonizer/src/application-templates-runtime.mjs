ccc7bcf67f53773e46a5d56a492877295119e289
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-07-31 10:22:57 -0400

    Restore unsaved editable starter state

--- applications/holofractal_harmonizer/src/application-templates-runtime.mjs
+++ applications/holofractal_harmonizer/src/application-templates-runtime.mjs
@@ -179,7 +179,7 @@ export function materializeApplicationTemplate(id) {
       name: path.split('/').at(-1),
       mediaType,
       content,
-      dirty: false,
+      dirty: true,
       checkpoint: `Created from ${template.label} starter`,
     })),
   };
