55cdf11fba7c3fce3264a81231e72f8cf126d6e1
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-17 12:25:37 -0400

    Pass 219 I112 compose stable admission lowering implementation

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -1,4 +1,4 @@
-/* Additive exact ABI aggregate: frozen v1.1 base + Pass 219 UQCEL 1.8 + Pass 192 Fibonacci composition 1.9 + Pass 219 RNA transcription 1.10 + Pass 219 RNA rule grammar 1.11. */
+/* Additive exact ABI aggregate: frozen v1.1 base + Pass 219 UQCEL 1.8 + Pass 192 Fibonacci composition 1.9 + Pass 219 RNA transcription 1.10 + Pass 219 RNA rule grammar 1.11 + Pass 219 stable VM81 admission lowering 1.12. */
 #include "hhs_runtime_exact_abi_v1_1_base.inc"
 #include "hhs_runtime_uqcel_1_8_bigint.inc"
 #include "hhs_runtime_uqcel_1_8_validate.inc"
@@ -6,3 +6,4 @@
 #include "hhs_pass192_fibonacci_compression_1_9.inc"
 #include "hhs_pass219_rna_transcription_1_10.inc"
 #include "hhs_pass219_rna_rule_grammar_1_11.inc"
+#include "hhs_pass219_rna_admission_lowering_1_12.inc"
