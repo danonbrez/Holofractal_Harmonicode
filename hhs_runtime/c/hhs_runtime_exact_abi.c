b0baabc010aa5b38951967f38d7415c3a4174644
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-17 08:29:58 -0400

    Pass 219 1.11: compose RNA rule grammar runtime

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -1,7 +1,8 @@
-/* Additive exact ABI aggregate: frozen v1.1 base + Pass 219 UQCEL 1.8 + Pass 192 Fibonacci composition 1.9 + Pass 219 RNA transcription 1.10. */
+/* Additive exact ABI aggregate: frozen v1.1 base + Pass 219 UQCEL 1.8 + Pass 192 Fibonacci composition 1.9 + Pass 219 RNA transcription 1.10 + Pass 219 RNA rule grammar 1.11. */
 #include "hhs_runtime_exact_abi_v1_1_base.inc"
 #include "hhs_runtime_uqcel_1_8_bigint.inc"
 #include "hhs_runtime_uqcel_1_8_validate.inc"
 #include "hhs_runtime_uqcel_1_8_receipt.inc"
 #include "hhs_pass192_fibonacci_compression_1_9.inc"
 #include "hhs_pass219_rna_transcription_1_10.inc"
+#include "hhs_pass219_rna_rule_grammar_1_11.inc"
