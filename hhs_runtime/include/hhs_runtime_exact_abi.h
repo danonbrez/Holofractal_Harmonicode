13a464d5ce028c3dce70c1f72c7e7447b0f6f10e
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-17 23:14:45 -0400

    Export Pass 216 alignment binding from exact ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -10,4 +10,5 @@
 #include "hhs_pass219_rna_execution_composer_1_14.h"
 #include "hhs_pass219_inherited_pass218_1_16.h"
 #include "hhs_pass219_inherited_pass217_1_16.h"
+#include "hhs_pass219_inherited_pass216_1_16.h"
 #endif
