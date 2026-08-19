27dc3ea90ced2d43128495b9ba2dd2a91342b6eb
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-19 11:10:00 -0400

    Pass 219 I123: register Pass 201 in exact ABI aggregate

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -25,6 +25,7 @@
 #include "hhs_pass219_inherited_pass204_1_20.h"
 #include "hhs_pass219_inherited_pass203_1_21.h"
 #include "hhs_pass219_inherited_pass202_1_22.h"
+#include "hhs_pass219_inherited_pass201_1_23.h"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.h"
 #include "hhs_pass219b_universal_phase_locality_1_0.h"
 #endif
