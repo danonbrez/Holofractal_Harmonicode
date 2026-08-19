9f14689b57dc3afe24e49fa891d14ce7728cb627
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-19 04:29:53 -0400

    Register universal phase-locality ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -22,4 +22,5 @@
 #include "hhs_pass219_inherited_pass207_1_17.h"
 #include "hhs_pass219_inherited_pass206_1_18.h"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.h"
+#include "hhs_pass219b_universal_phase_locality_1_0.h"
 #endif
