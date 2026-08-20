758d59d7dab55b09749f281d472fe146684cd779
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-20 12:24:10 -0400

    Pass 219B I6 repair: expose global relation bridge proof geometry

--- hhs_runtime/include/hhs_pass219b_global_zero_sum_closure_1_0.h
+++ hhs_runtime/include/hhs_pass219b_global_zero_sum_closure_1_0.h
@@ -8,13 +8,21 @@ extern "C" {
 #endif
 
 #define HHS_EXACT_PASS219B_ZERO_SUM_VERSION_MAJOR 1U
-#define HHS_EXACT_PASS219B_ZERO_SUM_VERSION_MINOR 0U
+#define HHS_EXACT_PASS219B_ZERO_SUM_VERSION_MINOR 1U
 #define HHS_EXACT_PASS219B_ZERO_SUM_VERSION_PATCH 0U
 #define HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES 32U
 #define HHS_EXACT_PASS219B_ZERO_SUM_UNIT_PERIMETER_COUNT 8U
+#define HHS_EXACT_PASS219B_ZERO_SUM_CELL_COUNT 81U
+#define HHS_EXACT_PASS219B_ZERO_SUM_LO_SHU_GROUP_COUNT 41U
+#define HHS_EXACT_PASS219B_ZERO_SUM_TRIT_COUNT 3U
+#define HHS_EXACT_PASS219B_ZERO_SUM_HYDRATION_SLOT_COUNT 5184U
+#define HHS_EXACT_PASS219B_ZERO_SUM_HYDRATION_STATE_COUNT UINT64_C(51648192)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PHASE_ORIGIN_COUNT 81U
+#define HHS_EXACT_PASS219B_ZERO_SUM_PHASE_PROJECTED_STATE_COUNT UINT64_C(4183503552)
 
 #define HHS_EXACT_PASS219B_ZERO_SUM_CENTER_RELATION "x+y+z+w=0"
 #define HHS_EXACT_PASS219B_ZERO_SUM_PHASE_RELATION "I+I^2+I^3+I^4=0"
+#define HHS_EXACT_PASS219B_GLOBAL_RECURSIVE_RELATION "N/D^4=D^4"
 
 #define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_CENTER UINT64_C(0x0001)
 #define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_PHASE_CARRIER UINT64_C(0x0002)
@@ -23,9 +31,12 @@ extern "C" {
 #define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_ZW_UNIT UINT64_C(0x0010)
 #define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_U72_UNIT_BINDING UINT64_C(0x0020)
 #define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_DENOMINATOR_PROJECTION UINT64_C(0x0040)
-#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_RECURSIVE_FIXED_POINT_REQUIRED UINT64_C(0x0080)
-#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_MONOLITHIC_CHAIN_REQUIRED UINT64_C(0x0100)
-#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_REQUIRED UINT64_C(0x01FF)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_GLOBAL_TENSOR_SOURCE UINT64_C(0x0080)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_PHASE_QUANTIZATION UINT64_C(0x0100)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_LO_SHU_QUDIT UINT64_C(0x0200)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_VM81_HYDRATION UINT64_C(0x0400)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_RECURSIVE_CLOSURE UINT64_C(0x0800)
+#define HHS_EXACT_PASS219B_ZERO_SUM_PROOF_REQUIRED UINT64_C(0x0FFF)
 
 typedef struct HHSExactPass219BGlobalZeroSumClosureV1 {
     uint32_t struct_size;
@@ -40,21 +51,41 @@ typedef struct HHSExactPass219BGlobalZeroSumClosureV1 {
     uint8_t u72_unit_projection_bound;
     uint8_t denominator_unit_perimeter_count;
     uint8_t denominator_center_zero_sum_preserved;
-    uint8_t recursive_fixed_point_required;
-    uint8_t monolithic_chain_required;
-    uint8_t full_monolithic_evaluated;
+    uint8_t global_tensor_source_bound;
+    uint8_t phase_quantization_bound;
+    uint8_t lo_shu_sudoku_qudit_bound;
+    uint8_t vm81_hydration_geometry_bound;
+    uint8_t recursive_closure_proven;
+    uint8_t global_relation_bridge_proven;
     uint8_t global_enforcement_required;
     uint8_t canonical_mutation_authority;
     uint8_t canonical_persistence_authority;
     uint8_t canonical_hash72_authority;
-    uint8_t reserved0[5];
+    uint8_t cell_count81;
+    uint8_t lo_shu_group_count41;
+    uint8_t trit_count3;
+    uint8_t phase_origin_count81;
+    uint16_t hydration_slot_count5184;
+    uint16_t reserved0;
+    uint64_t hydration_state_count;
+    uint64_t phase_projected_state_count;
     uint64_t proof_mask;
+    uint8_t global_tensor_source_sha256[HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES];
+    uint8_t phase_quantization_object_sha256[HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES];
     uint8_t closure_extension_sha256[HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES];
 } HHSExactPass219BGlobalZeroSumClosureV1;
 
 HHS_EXACT_API uint32_t hhs_exact_pass219b_global_zero_sum_version(void);
 
 HHS_EXACT_API HHSExactStatus hhs_exact_pass219b_global_zero_sum_source_sha256(
+    uint8_t out_sha256[HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES]
+);
+
+HHS_EXACT_API HHSExactStatus hhs_exact_pass219b_global_tensor_source_sha256(
+    uint8_t out_sha256[HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES]
+);
+
+HHS_EXACT_API HHSExactStatus hhs_exact_pass219b_phase_quantization_source_sha256(
     uint8_t out_sha256[HHS_EXACT_PASS219B_ZERO_SUM_SOURCE_SHA256_BYTES]
 );
 
