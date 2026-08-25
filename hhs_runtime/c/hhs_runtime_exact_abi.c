78dda81299418a86d20ec80b4edd1950822f43e0
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-25 13:00:53 -0400

    Register Pass196 I130 in cumulative exact ABI implementation

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -1,4 +1,4 @@
-/* Additive exact ABI aggregate through repaired Pass 197 inherited hydration calibration binding and Pass 219B universal phase locality. */
+/* Additive exact ABI aggregate through repaired Pass 196 inherited integrated-environment binding and Pass 219B universal phase locality. */
 #include "hhs_runtime_exact_abi_v1_1_base.inc"
 #include "hhs_runtime_uqcel_1_8_bigint.inc"
 #include "hhs_runtime_uqcel_1_8_validate.inc"
@@ -33,6 +33,7 @@
 #include "hhs_pass219_inherited_pass199_1_27.inc"
 #include "hhs_pass219_inherited_pass198_1_28.inc"
 #include "hhs_pass219_inherited_pass197_1_29.inc"
+#include "hhs_pass219_inherited_pass196_1_30.inc"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.inc"
 #include "hhs_pass219b_universal_phase_locality_1_0.inc"
 #include "hhs_pass219_octonion_runtime_1_19.inc"
