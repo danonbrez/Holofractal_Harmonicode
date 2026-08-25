539f5afdff062403e1b8e8074f307f9e4116e427
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-25 17:21:22 -0400

    test(pass219-i130): align Pass196 repair witness identity

--- tests/pass219/test_pass219_inherited_pass196_1_30.c
+++ tests/pass219/test_pass219_inherited_pass196_1_30.c
@@ -31,7 +31,7 @@ static HHSExactPass196RepairedIntegratedEnvironmentWitnessV1 witness(void) {
     strcpy(w.repaired_api_blob, "39187c3376591c64758019090d9b115c6a43f6ee");
     strcpy(w.repaired_frontend_blob, "1503903c844c9e601133853eed9ed597f6fd2274");
     strcpy(w.projection_refresh_blob, "44254e10f90e929a4f8c1a18a75b3ca14a2c05ed");
-    strcpy(w.repair_regression_blob, "55d1da0ea58044436646ccd8a331088135515c8f");
+    strcpy(w.repair_regression_blob, "d0860d89cd8abe596f49c73b7e544511cdaba5d0");
     strcpy(w.repair_workflow_blob, "7a19d3e7faab6e7210e156026300e96550b9afcb");
     return w;
 }
