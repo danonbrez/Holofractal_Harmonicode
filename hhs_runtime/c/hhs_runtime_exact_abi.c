a8525b15f50b597b663384fcc22693c2bfe8ea72
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-26 12:37:45 -0400

    Aggregate Pass 193 I133 exact C implementation

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -1,4 +1,4 @@
-/* Additive exact ABI aggregate through Pass 194 inherited storage/training binding and Pass 219B universal phase locality. */
+/* Additive exact ABI aggregate through Pass 193 inherited hypersolid/native-egress binding and Pass 219B universal phase locality. */
 #include "hhs_runtime_exact_abi_v1_1_base.inc"
 #include "hhs_runtime_uqcel_1_8_bigint.inc"
 #include "hhs_runtime_uqcel_1_8_validate.inc"
@@ -36,6 +36,7 @@
 #include "hhs_pass219_inherited_pass196_1_30.inc"
 #include "hhs_pass219_inherited_pass195_1_31.inc"
 #include "hhs_pass219_inherited_pass194_1_32.inc"
+#include "hhs_pass219_inherited_pass193_1_33.inc"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.inc"
 #include "hhs_pass219b_universal_phase_locality_1_0.inc"
 #include "hhs_pass219_octonion_runtime_1_19.inc"
@@ -43,4 +44,4 @@
 #include "hhs_pass219_authority_router_1_21_6.inc"
 #include "hhs_pass219_harmonicode_global_constraint_membrane_1_21_9.inc"
 #include "hhs_pass219b_selective_projection_1_0.inc"
-#include "hhs_pass219b_sparse_dirty_projection_1_0.inc"
+#include "hhs_pass219b_sparse_dirty_projection_1_0.inc"}
