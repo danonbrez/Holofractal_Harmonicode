45c0f0bc8969a4348b2404ff95cf7b9cfc9ce4cd
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-26 07:07:58 -0400

    Wire Pass 195 I131 into exact ABI implementation

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -1,4 +1,4 @@
-/* Additive exact ABI aggregate through repaired Pass 196 inherited integrated-environment binding and Pass 219B universal phase locality. */
+/* Additive exact ABI aggregate through repaired Pass 195 inherited Kimi K3 binding and Pass 219B universal phase locality. */
 #include "hhs_runtime_exact_abi_v1_1_base.inc"
 #include "hhs_runtime_uqcel_1_8_bigint.inc"
 #include "hhs_runtime_uqcel_1_8_validate.inc"
@@ -34,6 +34,7 @@
 #include "hhs_pass219_inherited_pass198_1_28.inc"
 #include "hhs_pass219_inherited_pass197_1_29.inc"
 #include "hhs_pass219_inherited_pass196_1_30.inc"
+#include "hhs_pass219_inherited_pass195_1_31.inc"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.inc"
 #include "hhs_pass219b_universal_phase_locality_1_0.inc"
 #include "hhs_pass219_octonion_runtime_1_19.inc"
@@ -41,4 +42,4 @@
 #include "hhs_pass219_authority_router_1_21_6.inc"
 #include "hhs_pass219_harmonicode_global_constraint_membrane_1_21_9.inc"
 #include "hhs_pass219b_selective_projection_1_0.inc"
-#include "hhs_pass219b_sparse_dirty_projection_1_0.inc"
+#include "hhs_pass219b_sparse_dirty_projection_1_0.inc"}
