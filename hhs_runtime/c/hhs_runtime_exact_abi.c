db84767341ccf3d25b1c0919063e8c7c5b29098e
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-31 11:30:43 -0400

    Wire H36 branch knowledge fabric into exact ABI

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -60,3 +60,4 @@
 #include "hhs_pass219_harmonic36_factorization_fabric_1_0.inc"
 #include "hhs_pass219_harmonic36_hash216_rna_binding_1_0.inc"
 #include "hhs_pass219_harmonic36_compression_gpu_fabric_1_0.inc"
+#include "hhs_pass219_harmonic36_branch_knowledge_fabric_1_0.inc"
