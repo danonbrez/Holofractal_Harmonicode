32f1d6450ed8285288ea52ac7938d1489062bb2f
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-08-31 11:05:07 -0400

    Repair and wire Harmonic36 cumulative ABI includes

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -55,4 +55,6 @@
 #include "hhs_pass219b_selective_projection_1_0.inc"
 #include "hhs_pass219b_sparse_dirty_projection_1_0.inc"
 #include "hhs_pass219_mandatory_genesis_scaling_1_22.inc"
-#include "hhs_pass219_harmonic36_nested_vm_1_0.inc"\n#include "hhs_pass219_harmonic36_default_binding_1_0.inc"
+#include "hhs_pass219_harmonic36_nested_vm_1_0.inc"
+#include "hhs_pass219_harmonic36_default_binding_1_0.inc"
+#include "hhs_pass219_harmonic36_factorization_fabric_1_0.inc"
