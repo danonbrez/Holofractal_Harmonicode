ecd8d7d9995201f7fe000ec628cbd62700ffdae4
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-01 13:51:46 -0400

    Repair H36 latency integration line encoding: hhs_runtime/include/hhs_runtime_exact_abi.h

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -63,5 +63,6 @@
 #include "hhs_pass219_harmonic36_ka10_monitor_profile_1_9.h"
 #include "hhs_pass219_harmonic36_stack_selection_1_10.h"
 #include "hhs_pass219_harmonic36_stack_selection_cache_1_11.h"
-#include "hhs_pass219_global_latency_policy_25_3_1_0.h"\n#include "hhs_pass219_harmonic36_global_latency_policy_1_16.h"
+#include "hhs_pass219_global_latency_policy_25_3_1_0.h"
+#include "hhs_pass219_harmonic36_global_latency_policy_1_16.h"
 #endif
