59739c1eb643db4a7a99853836ee6ee4e0c8eb45
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-02 06:57:45 -0400

    Extend cumulative exact ABI through Pass 179

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -49,6 +49,7 @@
 #include "hhs_pass219_inherited_pass182_1_44.h"
 #include "hhs_pass219_inherited_pass181_1_45.h"
 #include "hhs_pass219_inherited_pass180_1_46.h"
+#include "hhs_pass219_inherited_pass179_1_47.h"
 #include "hhs_pass219_global_canonical_defaults_1_0.h"
 #include "hhs_pass219_multimodal_optimization_generalization_1_0.h"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.h"
