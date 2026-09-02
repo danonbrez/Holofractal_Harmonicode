0a0efa0741893d1d1259596fa4cef5e2c52c1d12
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-02 08:50:56 -0400

    Expose Pass 177 through aggregate exact ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -51,6 +51,7 @@
 #include "hhs_pass219_inherited_pass180_1_46.h"
 #include "hhs_pass219_inherited_pass179_1_47.h"
 #include "hhs_pass219_inherited_pass178_1_48.h"
+#include "hhs_pass219_inherited_pass177_1_49.h"
 #include "hhs_pass219_global_canonical_defaults_1_0.h"
 #include "hhs_pass219_multimodal_optimization_generalization_1_0.h"
 #include "hhs_pass219b_phase_quantized_hydration_1_0.h"
