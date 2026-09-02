e7cad5942711580916d228048e343eb47ae11623
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-02 07:58:01 -0400

    Add Pass 178 I148 cumulative C++ conformance

---
+++ tests/pass219/test_pass219_inherited_pass178_1_48.cpp
@@ -0,0 +1,19 @@
+#include "hhs_pass219_inherited_pass178_1_48.hpp"
+#include <cassert>
+int main(){
+ using hhs::rna::InheritedPass178ExactPhysics;
+ static_assert(InheritedPass178ExactPhysics::pass_number()==178U);
+ static_assert(!InheritedPass178ExactPhysics::terminal_pass178_completion_claimed());
+ static_assert(InheritedPass178ExactPhysics::repair_forward_required());
+ static_assert(!InheritedPass178ExactPhysics::complete_historical_constraint_corpus());
+ static_assert(InheritedPass178ExactPhysics::remaining_terminal_category_count()==12U);
+ static_assert(InheritedPass178ExactPhysics::singleton_vm81_inherited());
+ static_assert(!InheritedPass178ExactPhysics::independent_vm81_authority());
+ static_assert(!InheritedPass178ExactPhysics::independent_hash72_commit_authority());
+ static_assert(!InheritedPass178ExactPhysics::hash216_mutation_authority());
+ static_assert(!InheritedPass178ExactPhysics::renderer_mutation_authority());
+ static_assert(!InheritedPass178ExactPhysics::gpu_mutation_authority());
+ static_assert(!InheritedPass178ExactPhysics::browser_mutation_authority());
+ static_assert(!InheritedPass178ExactPhysics::floating_point_canonical_authority());
+ return 0;
+}
