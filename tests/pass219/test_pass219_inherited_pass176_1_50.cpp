1f0e6c46d3fa1681ea0c586cb5cd51934d09e659
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-03 11:34:11 -0400

    Add C++ conformance test for Pass 176 I150 binding

---
+++ tests/pass219/test_pass219_inherited_pass176_1_50.cpp
@@ -0,0 +1,17 @@
+#include <cassert>
+#include "hhs_pass219_inherited_pass176_1_50.hpp"
+
+int main() {
+    using hhs::rna::InheritedPass176TerminalIDE;
+    static_assert(InheritedPass176TerminalIDE::pass_number() == 176U);
+    static_assert(InheritedPass176TerminalIDE::terminal_pass176_completion_claimed());
+    static_assert(InheritedPass176TerminalIDE::runtime_os_public_root_preserved());
+    static_assert(InheritedPass176TerminalIDE::additive_pass176_route_preserved());
+    static_assert(InheritedPass176TerminalIDE::singleton_vm81_inherited());
+    static_assert(InheritedPass176TerminalIDE::hash72_commit_streams() == 1U);
+    static_assert(!InheritedPass176TerminalIDE::independent_vm81_authority());
+    static_assert(!InheritedPass176TerminalIDE::independent_hash72_commit_authority());
+    static_assert(!InheritedPass176TerminalIDE::hash216_mutation_authority());
+    assert(InheritedPass176TerminalIDE::pass_number() == 176U);
+    return 0;
+}
