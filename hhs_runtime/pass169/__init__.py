2cf543bcb72a577c301d4d448931fe97cc959fc8
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-04 10:36:23 -0400

    Pass 219 I165: add Pass169 public service package

---
+++ hhs_runtime/pass169/__init__.py
@@ -0,0 +1,5 @@
+"""Pass 169 public algebra service surfaces."""
+
+from .public_service import Pass169AlgebraService, Pass169PublicSurfaceError
+
+__all__ = ["Pass169AlgebraService", "Pass169PublicSurfaceError"]
