4d5c76183340b5438b4aaa0cc8c8caed18c871a3
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-04 11:14:06 -0400

    Pass 219 I166: bind Pass168 into exact ABI implementation

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -82,3 +82,4 @@
 #include "hhs_pass219_harmonic36_branch_reference_cache_1_17.inc"
 #include "hhs_pass219_global_latency_policy_25_3_1_0.inc"
 #include "hhs_pass219_harmonic36_global_latency_policy_1_16.inc"
+#include "hhs_pass168_parameter_circuit_1_0.inc"
