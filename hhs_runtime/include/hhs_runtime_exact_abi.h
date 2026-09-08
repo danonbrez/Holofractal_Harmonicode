5299d39998a162eb7622075a18e03a09c8339a81
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-07 22:57:54 -0400

    Bind Phase11 symbolic phase-gear ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -72,6 +72,7 @@
 #include "hhs_pass219_hhcq_reciprocal_economy_1_29.h"
 #include "hhs_pass219_hhcq_prime_rational_expansion_1_30.h"
 #include "hhs_pass219_hhcq_8basis_equilibrium_parity_1_31.h"
+#include "hhs_pass219_hhcq_symbolic_phase_gear_1_32.h"
 #include "hhs_pass219_raw5184_octonion_audio_hydration_1_0.h"
 #include "hhs_pass219_audio_security_transport_1_0.h"
 #include "hhs_pass219_global_raw5184_serialization_hydration_1_0.h"
