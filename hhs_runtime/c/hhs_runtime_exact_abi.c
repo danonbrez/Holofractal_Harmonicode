9563351c5c917acd3dfd87f6bf46e0a372aa5317
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-07 21:25:58 -0400

    Bind Phase10 prime-rational expansion implementation

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -72,6 +72,7 @@
 #include "hhs_pass219_hhcq_temporal_cubic_1_28.inc"
 #include "hhs_pass219_hhcq_temporal_regret_1_28.inc"
 #include "hhs_pass219_hhcq_reciprocal_economy_1_29.inc"
+#include "hhs_pass219_hhcq_prime_rational_expansion_1_30.inc"
 #include "hhs_pass219_raw5184_octonion_audio_hydration_1_0.inc"
 #include "hhs_pass219_audio_security_transport_1_0.inc"
 #include "hhs_pass219_global_raw5184_serialization_hydration_1_0.inc"
