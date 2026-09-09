ee8c1d8b5f893dd8430549276ce07cd3c1fdce14
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-09 05:33:17 -0400

    pass219: restore standalone Pass168 comparator header contract

---
+++ hhs_runtime/c/hhs_pass168_comparator_1_0.h
@@ -0,0 +1,7 @@
+#ifndef HHS_PASS168_COMPARATOR_1_0_C_FORWARD_H
+#define HHS_PASS168_COMPARATOR_1_0_C_FORWARD_H
+
+/* Preserve the historical standalone -Ihhs_runtime/c ABI compile contract. */
+#include "../include/hhs_pass168_comparator_1_0.h"
+
+#endif
