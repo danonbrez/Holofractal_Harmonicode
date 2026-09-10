af5c99b8411d37f6204fc660fbc0685ca442af20
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-10 19:26:51 -0400

    Merge I182 geometry: reconcile aggregate ABI source

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -107,5 +107,6 @@
 #include "hhs_pass219_harmonic36_branch_reference_cache_1_17.inc"
 #include "hhs_pass219_global_latency_policy_25_3_1_0.inc"
 #include "hhs_pass219_harmonic36_global_latency_policy_1_16.inc"
+#include "hhs_pass219_i182_harmonic_geometry_membrane_1_0.inc"
 #include "hhs_pass168_parameter_circuit_1_0.inc"
 #include "hhs_pass168_comparator_1_0.inc"
