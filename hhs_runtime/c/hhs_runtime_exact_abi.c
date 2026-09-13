03d05bae14debd9c8472fe394a810d452f962d87
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-13 10:38:23 -0400

    Wire Lane 5 exact boundary quantum thermo into cumulative ABI

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -114,3 +114,4 @@
 #include "hhs_pass219_vm81_pqc_signature_1_31.inc"
 #include "hhs_pass219_vm81_environmental_recovery_1_32.inc"
 #include "hhs_pass219_lane5_global_holographic_nucleus_1_34.inc"
+#include "hhs_pass219_lane5_exact_boundary_quantum_thermo_1_35.inc"
