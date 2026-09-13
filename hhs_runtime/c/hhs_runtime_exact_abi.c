fccfda9130d825e5b1ec8f1e337fafc27b12ec15
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-13 09:41:09 -0400

    Pass 219: aggregate Lane 5 nucleus into exact ABI

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -113,3 +113,4 @@
 #include "hhs_pass219_vm81_pqc_firewall_1_30.inc"
 #include "hhs_pass219_vm81_pqc_signature_1_31.inc"
 #include "hhs_pass219_vm81_environmental_recovery_1_32.inc"
+#include "hhs_pass219_lane5_global_holographic_nucleus_1_34.inc"
