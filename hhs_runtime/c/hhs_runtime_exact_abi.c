f925cbe1c1109b7bc8eac5d750c5fe80705df4c2
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-14 14:17:29 -0400

    Compile Lane 5 automatic routing ABI 1.42

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -121,3 +121,4 @@
 #include "hhs_pass219_lane5_persistent_hash216_composition_memory_1_39.inc"
 #include "hhs_pass219_lane5_recursive_hash216_composition_graph_1_40.inc"
 #include "hhs_pass219_lane5_superedge_hierarchy_1_41.inc"
+#include "hhs_pass219_lane5_automatic_superedge_routing_1_42.inc"
