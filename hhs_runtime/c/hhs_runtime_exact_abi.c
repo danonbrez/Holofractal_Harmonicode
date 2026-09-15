70d595529abe3bc93c0de98afd24a19dcbc2cc8d
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-15 01:42:09 -0400

    Compile repository capability reverse discovery into exact ABI

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -123,3 +123,4 @@
 #include "hhs_pass219_lane5_superedge_hierarchy_1_41.inc"
 #include "hhs_pass219_lane5_automatic_superedge_routing_1_42.inc"
 #include "hhs_pass219_lane5_executable_capability_self_model_1_43.inc"
+#include "hhs_pass219_lane5_repository_capability_reverse_discovery_1_44.inc"
