9ec232896e16323a5ddb9e3ddb720ffb9901daaf
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-15 06:18:18 -0400

    Expose Hash216 fractal qudit proof hydration in exact ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -105,4 +105,5 @@
 #include "hhs_pass219_lane5_automatic_superedge_routing_1_42.h"
 #include "hhs_pass219_lane5_executable_capability_self_model_1_43.h"
 #include "hhs_pass219_lane5_repository_capability_reverse_discovery_1_44.h"
+#include "hhs_pass219_hash216_fractal_qudit_admission_1_45.h"
 #endif
