ebd1febeae3a2b69bcb893be2ff1ff2a624f3f20
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-26 11:20:44 -0400

    Pass 219: compile P^x2 manifold 1.64 into VM81 runtime

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -114,6 +114,7 @@
 #include "hhs_pass219_vm81_pqc_firewall_1_30.inc"
 #include "hhs_pass219_vm81_pqc_signature_1_31.inc"
 #include "hhs_pass219_lane5_hnan_global_constraint_1_63.inc"
+#include "hhs_pass219_p_x2_global_reciprocal_manifold_1_64.inc"
 #include "hhs_pass219_vm81_environmental_recovery_1_32.inc"
 #include "hhs_pass219_lane5_global_holographic_nucleus_1_34.inc"
 #include "hhs_pass219_lane5_exact_boundary_quantum_thermo_1_35.inc"
