bfdf1aa6d603b0dd77213d278d6e0d15affd1585
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-26 10:13:08 -0400

    Pass 219: expose HNAN global constraint in exact ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -96,6 +96,7 @@
 #include "hhs_pass219_i182_harmonic_geometry_membrane_1_0.h"
 #include "hhs_pass168_parameter_circuit_1_0.h"
 #include "hhs_pass219_lane5_global_holographic_nucleus_1_34.h"
+#include "hhs_pass219_lane5_hnan_global_constraint_1_63.h"
 #include "hhs_pass219_lane5_exact_boundary_quantum_thermo_1_35.h"
 #include "hhs_pass219_delta_reciprocal_constructor_1_36.h"
 #include "hhs_pass219_lane5_hash216_gpu_phase_interlace_1_37.h"
