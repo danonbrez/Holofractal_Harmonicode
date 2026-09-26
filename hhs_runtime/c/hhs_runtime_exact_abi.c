f3f350220e925304d69443d843eec3b04502bffe
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-26 10:13:10 -0400

    Pass 219: compile HNAN resolver into VM81 exact runtime

--- hhs_runtime/c/hhs_runtime_exact_abi.c
+++ hhs_runtime/c/hhs_runtime_exact_abi.c
@@ -113,6 +113,7 @@
 #include "hhs_pass168_comparator_1_0.inc"
 #include "hhs_pass219_vm81_pqc_firewall_1_30.inc"
 #include "hhs_pass219_vm81_pqc_signature_1_31.inc"
+#include "hhs_pass219_lane5_hnan_global_constraint_1_63.inc"
 #include "hhs_pass219_vm81_environmental_recovery_1_32.inc"
 #include "hhs_pass219_lane5_global_holographic_nucleus_1_34.inc"
 #include "hhs_pass219_lane5_exact_boundary_quantum_thermo_1_35.inc"
