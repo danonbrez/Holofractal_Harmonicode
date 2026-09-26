2079cd029dacabc2973a31b45863f07ba7a817d4
danonbrez <153354844+danonbrez@users.noreply.github.com>
2026-09-26 11:55:51 -0400

    Pass 219: expose polarity s half-turn 1.65 through exact ABI

--- hhs_runtime/include/hhs_runtime_exact_abi.h
+++ hhs_runtime/include/hhs_runtime_exact_abi.h
@@ -98,6 +98,7 @@
 #include "hhs_pass219_lane5_global_holographic_nucleus_1_34.h"
 #include "hhs_pass219_lane5_hnan_global_constraint_1_63.h"
 #include "hhs_pass219_p_x2_global_reciprocal_manifold_1_64.h"
+#include "hhs_pass219_polarity_s_half_turn_1_65.h"
 #include "hhs_pass219_lane5_exact_boundary_quantum_thermo_1_35.h"
 #include "hhs_pass219_delta_reciprocal_constructor_1_36.h"
 #include "hhs_pass219_lane5_hash216_gpu_phase_interlace_1_37.h"
